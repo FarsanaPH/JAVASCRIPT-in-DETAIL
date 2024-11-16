8ea1b765a9aca25a99d6eb13438b877025bc9da1
FarsanaPH <farsanaph2001@gmail.com>
2024-11-17 00:41:15 +0530

    conversion nd coercion

---
+++ 3-TYPE CASTING/1-conversion_VS_coercion.js
@@ -0,0 +1,28 @@
+//                                          TYPE CONVERSION-  TYPE COERCION VS TYPE CASTING
+// Type conversion is the conversion of values from one data type to another. It can be implicit or explicit.
+// Type coercion is the automatic or implicit conversion of values from one data type to another (such as numbers to strings).
+// Type casting generally refers to explicit conversion of values from one data type to another using functions or operators
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// TYPE CONVERSION - conversion of values from one data type to another. It can be either implicit or explicit.
+
+// 1)IMPLICIT TYPE CONVERSION (type coercion [Type Juggling] )
+// automatic or implicit conversion of values from one data type to another
+// In this,the conversion from one data type to another done by the language automatically during runtime without developer intervention.
+// eg: In "foo" + 1, the Number 1 is implicitly converted into a String and the expression returns "foo1"
+// let sum = "5" + 9;
+// console.log(sum);  // "59"
+
+// 2)EXPLICIT TYPE CONVERSION (Type Casting)
+// In this, the programmer manually converts a value from one type to another using functions or operators, like Number("123") ,parseInt("456") , parseFloat() or toString().
+// eg: In expression, Number("0x11")- the string "0x11" is explicitly converted to the number 17 using certain operators manually.
+// let sum = Number("5") + 9;
+// console.log(sum);  // 14
+
+
+
+
+
+
+
+
