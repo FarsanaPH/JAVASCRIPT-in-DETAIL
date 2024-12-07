0f24498506723893cab7aa0293e6209adba84589
FarsanaPH <farsanaph2001@gmail.com>
2024-12-07 13:11:52 +0530

    if else

---
+++ 7-CONTROL FLOW/1-CONDITIONAL  STATEMENTS/2-if...else.js
@@ -0,0 +1,26 @@
+// if...else 
+
+// SYNTAX-
+// if (condition) {
+//     statement1;
+// } else {
+//     statement2;
+// }
+
+// let year = prompt('In which year was the ECMAScript-2015 specification published?', '');
+// if (year == 2015) {
+//   console.log( 'You guessed it right!' );
+// } else {
+//   console.log( 'How can you be so wrong?' ); // any value except 2015
+// }
+
+
+// case: if....else if...else -
+// let year = prompt('In which year was the ECMAScript-2015 specification published?', '');
+// if (year < 2015) {
+//   console.log( 'Too early...' );
+// } else if (year > 2015) {
+//   console.log( 'Too late' );
+// } else {
+//   console.log( 'Exactly!' );
+// }
