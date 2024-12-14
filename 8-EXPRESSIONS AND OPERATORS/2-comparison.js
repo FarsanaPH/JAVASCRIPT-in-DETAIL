d462e64da1f26690a2043e5de749c94bc68ab819
FarsanaPH <farsanaph2001@gmail.com>
2024-12-14 20:19:10 +0530

    comparison

---
+++ 8-EXPRESSIONS AND OPERATORS/2-comparison.js
@@ -0,0 +1,11 @@
+// Comparison Operators: Used to compare values and return a boolean. 
+// include operations like (==, ===, !=, <, >, etc.).
+
+// console.log(5 == '5');   // true (equality without type checking)
+// console.log(5 === '5');  // false (equality with type checking)
+// console.log(6 != '6');   // false (equal without type checking)
+// console.log(6 !== '6');  // true (not equal)
+// console.log(5 > 3);      // true
+// console.log(5 < 3);      // false
+// console.log(5 <= 5);     // true
+// console.log(6 >= 5);     // true
