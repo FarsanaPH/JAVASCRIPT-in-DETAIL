851c36cdd9e7d4a528ef7d4bc92e17db126f6c7c
FarsanaPH <farsanaph2001@gmail.com>
2024-12-15 22:28:41 +0530

    arithmetic

---
+++ 8-EXPRESSIONS AND OPERATORS/3-arithmetic.js
@@ -0,0 +1,11 @@
+// Arithmetic Operators: Used for mathematical operations.
+// include operations like (+, -, *, /, %, **).
+
+// let x = 10;
+// let y = 3;
+// console.log(x + y);   // 13 (addition)
+// console.log(x - y);   // 7 (subtraction)
+// console.log(x * y);   // 30 (multiplication)
+// console.log(x / y);   // 3.333... (division)
+// console.log(x % y);   // 1 (modulus)
+// console.log(x ** y);  // 1000 (exponentiation)
