5f19cb7caa7eef4f37a2cc3db92a8138284313d7
FarsanaPH <farsanaph2001@gmail.com>
2024-12-16 16:46:57 +0530

    logical

---
+++ 8-EXPRESSIONS AND OPERATORS/4-logical.js
@@ -0,0 +1,19 @@
+// Logical Operators: Return a boolean based on logic 
+// include operations like (&&, ||, !)
+
+// // Logical Operators: Used for logical operations.
+// Logical AND (&&) - Returns true only if both operands are true
+// console.log(true && true);   // true
+// console.log(true && false);  // false
+// console.log(false && true);  // false
+// console.log(false && false); // false
+
+// Logical OR (||) - Returns true if at least one operand is true
+// console.log(true || true);   // true
+// console.log(true || false);  // true
+// console.log(false || true);  // true
+// console.log(false || false); // false
+
+// Logical NOT (!) - Inverts the boolean value
+// console.log(!true);  // false
+// console.log(!false); // true
