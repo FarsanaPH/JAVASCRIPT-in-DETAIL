513f79464a5af6176822bea80068b319a2e3c0a7
FarsanaPH <farsanaph2001@gmail.com>
2024-12-21 01:11:55 +0530

    bigint

---
+++ 8-EXPRESSIONS AND OPERATORS/9-bigint.js
@@ -0,0 +1,15 @@
+// BigInt Operators: 
+// Used for operations with large integers.
+
+// BigInt addition
+// const a = 1n + 2n; // 3n
+// Division with BigInts round towards zero
+// const b = 1n / 2n; // 0n
+
+// const a = Number(1n) + 2; // 3
+// const b = 1n + BigInt(2); // 3n
+
+// const a = 1n > 2; // false
+// const b = 3 > 2n; // true
+
+
